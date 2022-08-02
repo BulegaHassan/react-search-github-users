14ad83fa512ddccba64b132e277bf363bb586bc9
Hassan Bulega <hassanbulega@gmail.com>
2022-08-02 12:11:17 +0300

    feat: set up usestate

--- src/context/context.js
+++ src/context/context.js
@@ -1,18 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
 
-const GithubContext = React.createContext()
+const GithubContext = React.createContext();
 
-const GithubProvider = ({children}) => {
-    return (
-        <GithubContext.Provider value={'hey'}>
-            {children}
-        </GithubContext.Provider>
-    )
-}
-export {GithubContext, GithubProvider}
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  // request, loading
+  const[requests,setRequests]=useState(0)
+  const[loading,setLoading]=useState(false)
+  // error
+
+  useEffect(()=> {
+    console.log('hey uploaded');
+  },[])
+
+  return (
+    <GithubContext.Provider value={{
+        githubUser,
+        repos,
+        followers,
+    }}>{children}</GithubContext.Provider>
+  );
+};
+export { GithubContext, GithubProvider };
